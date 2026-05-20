e9bf49ee3d5bd3dbe582ae2988756f5c06d8d938
Chayanan <Chayanan.mari@dome.tu.ac.th>
2026-05-20 21:07:38 +0700

    add screenshots and edit supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,8 @@
-const supabaseUrl = "https://pdagpmphyselovmbjyll.supabase.co";
+const SUPABASE_URL = "https://pdagpmphyselovmbjyll.supabase.co";
 
-const supabaseKey = "sb_publishable_rO_Dq6AGTPdn6aq1PFwYaw_MPEwBxT1";
+const SUPABASE_ANON_KEY = "sb_publishable_rO_Dq6AGTPdn6aq1PFwYaw_MPEwBxT1";
 
-const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+);
